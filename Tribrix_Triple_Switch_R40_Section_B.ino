42570d004d87e642e8fe56bc19f7dcb3bf5be4f5
JakubKosiorek <jakubkosiorek@gmail.com>
2021-11-23 12:43:38 +0100

    Update Tribrix_Triple_Switch_R40_Section_B.ino

--- Tribrix_Triple_Switch_R40_Section_B.ino
+++ Tribrix_Triple_Switch_R40_Section_B.ino
@@ -1,23 +1,20 @@
 // LEGO_train_track_SECTION_B - TRIBRIX TRIPLE SWITCH R40
 
 
-//BIBLIOTEKI
+//LIBRARIES
+
 #include<Servo.h>
 
-//SEMAFORY
-#define led1RED   7
-#define led1GREEN 8
-#define led2RED   9
-#define led2GREEN 10
-#define led3RED   11
-#define led3GREEN 12
 
-//CZUJNIKI
+//SENSORS
+
 #define sensor_Tor1  2
 #define sensor_Tor2  3
 #define sensor_Tor3  4
 
-//SERVA
+
+//SERVO
+
 Servo servoL;
 Servo servoP;
 int servoL_Pin = 5;
@@ -25,44 +22,29 @@ int servoP_Pin = 6;
 int posServoL = 58;
 int posServoP = 100;
 
-//ZMIENNE
+
+//VARIABLES
+
 int delayIsSensorLow = 20;
 int data;
 
 
-
-//ZEGAR
-unsigned long czas;
-
-
-//USTAWIENIA POCZĄTKOWE DLA SEKCJI B
-void setup() {
+void setup() {    //INITIAL SETTINGS
 
   //COMM
   Serial.begin(38400);
   delay(1000);
-  Serial.println(92000);
-  
-  //OUTPUTS
-  pinMode(led1RED,    OUTPUT);
-  pinMode(led1GREEN,  OUTPUT);
-  pinMode(led2RED,    OUTPUT);
-  pinMode(led2GREEN,  OUTPUT);
-  pinMode(led3RED,    OUTPUT);
-  pinMode(led3GREEN,  OUTPUT);
-
+  Serial.println(9200);
+  delay(500);
+ 
   //INPUTS
   pinMode(sensor_Tor1, INPUT_PULLUP);
   pinMode(sensor_Tor2, INPUT_PULLUP);
   pinMode(sensor_Tor3, INPUT_PULLUP);
+  
 
   //SETTINGS
-  digitalWrite(led1RED,   LOW);
-  digitalWrite(led1GREEN, HIGH);
-  digitalWrite(led2RED,   HIGH);
-  digitalWrite(led2GREEN, LOW);
-  digitalWrite(led3RED,   LOW);
-  digitalWrite(led3GREEN, HIGH);
+
   servoL.attach(servoL_Pin);
   servoP.attach(servoP_Pin);
   servoL.write(58);
@@ -77,12 +59,10 @@ void TxRxData(){
   
   data = Serial.parseInt();
   delay(10);
-//  if (data == 1000){
-//    Serial.println(92000);
-//  }
+
 
   //  ROZJAZD
-  if (data == 1011){                //TOR 1 servoL na wprost, servoP na bok
+  if (data == 1013){  //TOR 1 servoL na wprost, servoP na bok
     servoL.attach(servoL_Pin);
     servoP.attach(servoP_Pin);
     for(posServoL =100; posServoL >58; posServoL -=1){
@@ -96,16 +76,9 @@ void TxRxData(){
     delay(100);
     servoL.detach();
     servoP.detach();
-    digitalWrite(led1RED,   HIGH);
-    digitalWrite(led1GREEN, LOW);
-    digitalWrite(led2RED,   LOW);
-    digitalWrite(led2GREEN, HIGH);
-    digitalWrite(led3RED,   LOW);
-    digitalWrite(led3GREEN, HIGH);
-    Serial.println(92011);
-  } //end 1011
+  } //end 1013
 
-  if (data == 1012){                //TOR 2 servoL na wprost, servoP na wprost
+  if (data == 1012){  //TOR 2 servoL na wprost, servoP na wprost
     servoL.attach(servoL_Pin);
     servoP.attach(servoP_Pin);
     for(posServoL =100; posServoL >58; posServoL -=1){
@@ -119,16 +92,9 @@ void TxRxData(){
     delay(100);
     servoL.detach();
     servoP.detach();
-    digitalWrite(led1RED,   LOW);
-    digitalWrite(led1GREEN, HIGH);
-    digitalWrite(led2RED,   HIGH);
-    digitalWrite(led2GREEN, LOW);
-    digitalWrite(led3RED,   LOW);
-    digitalWrite(led3GREEN, HIGH);
-    Serial.println(92012);
   } //end 1012
 
-  if (data == 1013){                //TOR 3 servoL na bok, servoP na wprost
+  if (data == 1011){  //TOR 3 servoL na bok, servoP na wprost
     servoL.attach(servoL_Pin);
     servoP.attach(servoP_Pin);
     for(posServoL =58; posServoL <100; posServoL +=1){
@@ -142,51 +108,40 @@ void TxRxData(){
     delay(100);
     servoL.detach();
     servoP.detach();
-    digitalWrite(led1RED,   LOW);
-    digitalWrite(led1GREEN, HIGH);
-    digitalWrite(led2RED,   LOW);
-    digitalWrite(led2GREEN, HIGH);
-    digitalWrite(led3RED,   HIGH);
-    digitalWrite(led3GREEN, LOW);
-    Serial.println(92013);
   } //end 1013
 
-
-  
+//  if (digitalRead(sensor_Tor1) == LOW){    //ZMIANA SEMAFOR 1 NA ZIELONE
+//    Serial.println(92011);
+//    delay(500);
+//    } // end sensor_Tor1 == LOW
 } // end void TxRxData()
 
 
-void loop() {
+void loop() { //  WORK IN LOOP
 
- czas = millis();
- if(Serial.available() > 0) TxRxData();
+
+ if(Serial.available() > 0) TxRxData(); //PRZEJŚCIE DO FUNKCJI void TxRxData()
   
 
-   if (digitalRead(sensor_Tor1) == LOW){
-    digitalWrite(led1RED,   LOW);
-    digitalWrite(led1GREEN, HIGH);
-    Serial.println(92021);
+ if (digitalRead(sensor_Tor1) == LOW){    //ZMIANA SEMAFOR 1 NA ZIELONE
+    Serial.println(92011);
     delay(500);
     } // end sensor_Tor1 == LOW
-   
-   if (digitalRead(sensor_Tor2) == LOW){
-    digitalWrite(led2RED,   LOW);
-    digitalWrite(led2GREEN, HIGH);
-    Serial.println(92022);
+
+ if (digitalRead(sensor_Tor2) == LOW){    //ZMIANA SEMAFOR 1 NA ZIELONE
+    Serial.println(92012);
     delay(500);
     } // end sensor_Tor2 == LOW
 
-   if (digitalRead(sensor_Tor3) == LOW){
-    digitalWrite(led3RED,   LOW);
-    digitalWrite(led3GREEN, HIGH);
-    Serial.println(92023);
+ if (digitalRead(sensor_Tor3) == LOW){    //ZMIANA SEMAFOR 1 NA ZIELONE
+    Serial.println(92013);
     delay(500);
     } // end sensor_Tor3 == LOW
 
 } //  end void loop()
 
-// ELIMINACJA ZJAWISKA DRGAŃ STYKÓW
-bool isSensorLow(int sensor){
+
+bool isSensorLow(int sensor){ //CONTACT VIBRATION PHENOMENO ELIMINATION
   if (digitalRead(sensor) == LOW){
     delay(delayIsSensorLow);
     if (digitalRead(sensor) == LOW){
