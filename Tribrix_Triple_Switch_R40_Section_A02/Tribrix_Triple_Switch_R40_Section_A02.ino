c54e4825eee7555190d86846d26b7bdb47cf9518
JakubKosiorek <jakubkosiorek@gmail.com>
2021-12-14 23:54:11 +0100

    Update Tribrix_Triple_Switch_R40_Section_A02.ino

--- Tribrix_Triple_Switch_R40_Section_A02/Tribrix_Triple_Switch_R40_Section_A02.ino
+++ Tribrix_Triple_Switch_R40_Section_A02/Tribrix_Triple_Switch_R40_Section_A02.ino
@@ -1,33 +1,18 @@
 // LEGO_train_track_SECTION_A02 - TRIBRIX TRIPLE SWITCH R40
 
-
-//LIBRARIES
-
 #include<Servo.h>
-
-
-//SENSORS
-
 #define sensorTrack1  2
 #define sensorTrack2  3
 #define sensorTrack3  4
-
-
-//SERVO
-
 Servo servoL;
 Servo servoR;
 int servoL_Pin = 5;
 int servoR_Pin = 6;
 int posServoL = 58;
 int posServoR = 100;
-int trackStatus[3] = {1, 1, 1};
-
-//VARIABLES
-
 int delayIsSensorLow = 20;
 long data;
-
+int trackStatus[3];
 
 void setup() {                  // INITIAL SETTINGS
 
@@ -42,27 +27,28 @@ void setup() {                  // INITIAL SETTINGS
   
   //SETTINGS
   Serial.println("SECTION A02 ON-LINE");
+  Serial.print("SECTION A02 STATUS        | ");
   sectionStatus();                    // EXECUTE void sectionStatus()
-  TRACK_02_setUp();                   // EXECUTE void TRACK_02_setUp()
   
 } // end void setup()
 
+void sectionStatus(){           // SECTION A02 STATUS
+  
+  Serial.print(trackStatus[0]);
+  Serial.print(" | ");
+  Serial.print(trackStatus[1]);
+  Serial.print(" | ");
+  Serial.print(trackStatus[2]);
+  Serial.println(" | ");
+  
+} // end void sectionStatus()
+
 void sensorLow(){               // SEKTION A02 SENSORS WORK
-  if (isSensorLow(sensorTrack1)){   // SENSOR TRACK 01 IMPULSE -> TRACK 01 EMPTY
-    TRACK_01_setUp();
-    Serial.println(91213);
-    delay(100);
-  }
-  if (isSensorLow(sensorTrack2)){   // SENSOR TRACK 02 IMPULSE -> TRACK 02 STATUS = 0
-    TRACK_02_setUp();
-    Serial.println(91223);
-    delay(100);
-  }
-  if (isSensorLow(sensorTrack3)){   // SENSOR TRACK 03 IMPULSE -> TRACK 03 EMPTY
-    TRACK_03_setUp();
-    Serial.println(91233);
-    delay(100);
-  }
+  
+  if (isSensorLow(sensorTrack1)) TRACK_01_setUp();   // SENSOR TRACK 01 IMPULSE
+  if (isSensorLow(sensorTrack2)) TRACK_02_setUp();   // SENSOR TRACK 02 IMPULSE
+  if (isSensorLow(sensorTrack3)) TRACK_03_setUp();   // SENSOR TRACK 03 IMPULSE
+  
 } // end void sensorLow()
 
 void TxRxData(){                // TxRxData TRANSMISION
@@ -73,10 +59,11 @@ void TxRxData(){                // TxRxData TRANSMISION
   if (data == 1013) TRACK_01_setUp();  // TRACK 1 ON-LINE
   if (data == 1023) TRACK_02_setUp();  // TRACK 2 ON-LINE
   if (data == 1033) TRACK_03_setUp();  // TRACK 3 ON-LINE
-
+  
 } // end void TxRxData()
 
 void TRACK_01_setUp(){          // TRACK 01 ON-LINE
+  
   servoL.attach(servoL_Pin);                          // TUTN ON SERVO L
   servoR.attach(servoR_Pin);                          // TUTN ON SERVO R
   for(posServoL =58; posServoL <100; posServoL +=1){  // SERVO L ON THE POSITION
@@ -88,14 +75,17 @@ void TRACK_01_setUp(){          // TRACK 01 ON-LINE
   delay(100);
   servoL.detach();                                    // TUTN OFF SERVO L
   servoR.detach();                                    // TUTN OFF SERVO R
-  Serial.println("TRACK 01 SWITCHED ON-LINE");
   trackStatus[0] = 0;                                 // TRACK 01 STATUS ON-LINE  
   trackStatus[1] = 1;                                 // TRACK 02 STATUS OFF-LINE
   trackStatus[2] = 1;                                 // TRACK 03 STATUS OFF-LINE  
+  Serial.print("TRACK 01 SWITCHED ON-LINE | ");
   sectionStatus();
+  Serial.println(91210);
+  
 } // end void TRACK_01_setUp()
 
 void TRACK_02_setUp(){          // TRACK 02 ON-LINE
+  
   servoL.attach(servoL_Pin);                          // TUTN ON SERVO L
   servoR.attach(servoR_Pin);                          // TUTN ON SERVO R
   for(posServoL =100; posServoL >58; posServoL -=1){  // SERVO L ON THE POSITION
@@ -107,14 +97,17 @@ void TRACK_02_setUp(){          // TRACK 02 ON-LINE
   delay(100);
   servoL.detach();                                    // TUTN OFF SERVO L
   servoR.detach();                                    // TUTN OFF SERVO R
-  Serial.println("TRACK 02 SWITCHED ON-LINE");
-  trackStatus[0] = 1;                                 // TRACK 01 STATUS OFF-LINE  
+  trackStatus[0] = 1;                                 // TRACK 01 STATUS OFF-LINE
   trackStatus[1] = 0;                                 // TRACK 02 STATUS ON-LINE
-  trackStatus[2] = 1;                                 // TRACK 03 STATUS OFF-LINE  
+  trackStatus[2] = 1;                                 // TRACK 03 STATUS OFF-LINE
+  Serial.print("TRACK 02 SWITCHED ON-LINE | ");
   sectionStatus();
+  Serial.println(91220);
+  
 } // end void TRACK_02_setUp()
 
 void TRACK_03_setUp(){          // TRACK 03 ON-LINE
+  
   servoL.attach(servoL_Pin);                          // TUTN ON SERVO L
   servoR.attach(servoR_Pin);                          // TUTN ON SERVO R
   for(posServoL =100; posServoL >58; posServoL -=1){  // SERVO L ON THE POSITION
@@ -126,21 +119,14 @@ void TRACK_03_setUp(){          // TRACK 03 ON-LINE
   delay(100);
   servoL.detach();                                    // TUTN OFF SERVO L
   servoR.detach();                                    // TUTN OFF SERVO R
-  Serial.println("TRACK 03 SWITCHED ON-LINE");
-  trackStatus[0] = 1;                                 // TRACK 01 STATUS ON-LINE  
+  trackStatus[0] = 1;                                 // TRACK 01 STATUS ON-LINE
   trackStatus[1] = 1;                                 // TRACK 02 STATUS OFF-LINE
-  trackStatus[2] = 0;                                 // TRACK 03 STATUS OFF-LINE  
+  trackStatus[2] = 0;                                 // TRACK 03 STATUS OFF-LINE
+  Serial.print("TRACK 03 SWITCHED ON-LINE | ");
   sectionStatus();
+  Serial.println(91230);
+  
 } // end void TRACK_03_setUp()
-
-void sectionStatus(){           // SECTION A02 STATUS
-  Serial.println("SECTION A02 STATUS");
-  Serial.print(trackStatus[0]);
-  Serial.print(" | ");
-  Serial.print(trackStatus[1]);
-  Serial.print(" | ");
-  Serial.println(trackStatus[2]);
-} // end void sectionStatus()
 
 void loop() {                   // WORK IN LOOP
 
@@ -153,6 +139,7 @@ void loop() {                   // WORK IN LOOP
 } //  end void loop()
 
 bool isSensorLow(int sensor){   // CONTACT VIBRATION PHENOMENO ELIMINATION
+  
 if (digitalRead(sensor) == LOW){
     delay(delayIsSensorLow);
     if (digitalRead(sensor) == LOW){
@@ -160,4 +147,5 @@ if (digitalRead(sensor) == LOW){
     }
   }
   return false;
+  
 } // end bool isSensorLow()
