7f3d36d71fd6ab4d7cd307444d7cb3ec1668c9f4
JakubKosiorek <jakubkosiorek@gmail.com>
2021-12-15 16:15:06 +0100

    Update Tribrix_Triple_Switch_R40_Section_A01.ino

--- Tribrix_Triple_Switch_R40_Section_A01/Tribrix_Triple_Switch_R40_Section_A01.ino
+++ Tribrix_Triple_Switch_R40_Section_A01/Tribrix_Triple_Switch_R40_Section_A01.ino
@@ -24,7 +24,6 @@ int led2GREENflashing = 0;
 int led3GREENflashing = 0;
 unsigned long time;
 unsigned long flashTime = 500;
-int trackStatus[3];
 
 void setup() {                  // SEKTION A01 INITIAL SETTINGS
 
@@ -60,54 +59,26 @@ void setup() {                  // SEKTION A01 INITIAL SETTINGS
   digitalWrite(led3RED,   HIGH);
   digitalWrite(led3GREEN, HIGH);
   Serial.println("SECTION A01 ON-LINE");
-  Serial.print("SECTION A01 STATUS        | ");
-  sectionStatus();                    // EXECUTE void sectionStatus()
 
 } //  end void setup()
-
-void sectionStatus(){           // SECTION A01 STATUS
-  
-  Serial.print(trackStatus[0]);
-  Serial.print(" | ");
-  Serial.print(trackStatus[1]);
-  Serial.print(" | ");
-  Serial.print(trackStatus[2]);
-  Serial.println(" | ");
-  
-} // end void sectionStatus()
 
 void sensorLow() {              // SEKTION A01 SENSORS WORK
   
   if(isSensorLow(sensorTrack1)){      // SENSOR TRACK 01 IMULSE -> 91113
-    if (trackStatus[0] == 0){
-      trackStatus[0] = 1;               // TRACK 01 STATUS OFF-LINE
-      led1GREENflashing = 1;            // SEMAPHOR TRACK 01 GREEN START FLASHING
-      digitalWrite(led1RED, LOW);       // SEMAPHOR TRACK 01 RED OFF
-      Serial.print("SENSOR TRACK 01 IMULSE    | ");
-      sectionStatus();
-      Serial.println(91113);
+    led1GREENflashing = 1;            // SEMAPHOR TRACK 01 GREEN START FLASHING
+    digitalWrite(led1RED, LOW);       // SEMAPHOR TRACK 01 RED OFF
+    Serial.println(91113);
     }
-  }
   if(isSensorLow(sensorTrack2)){      // SENSOR TRACK 02 IMULSE -> 91123
-    if (trackStatus[1] == 0){    
-      trackStatus[1] = 1;               // TRACK 02 STATUS OFF-LINE
-      led2GREENflashing = 1;            // SEMAPHOR TRACK 02 GREEN START FLASHING
-      digitalWrite(led2RED, LOW);       // SEMAPHOR TRACK 02 RED OFF
-      Serial.print("SENSOR TRACK 02 IMULSE    | ");
-      sectionStatus();
-      Serial.println(91123);
+    led2GREENflashing = 1;            // SEMAPHOR TRACK 02 GREEN START FLASHING
+    digitalWrite(led2RED, LOW);       // SEMAPHOR TRACK 02 RED OFF
+    Serial.println(91123);
     }
-  }
   if(isSensorLow(sensorTrack3)){      // SENSOR TRACK 03 IMULSE -> 91133
-    if (trackStatus[2] == 0){        
-      trackStatus[2] = 1;               // TRACK 03 STATUS OFF-LINE
-      led3GREENflashing = 1;            // SEMAPHOR TRACK 03 GREEN START FLASHING
-      digitalWrite(led3RED, LOW);       // SEMAPHOR TRACK 03 RED OFF
-      Serial.print("SENSOR TRACK 03 IMULSE    | ");
-      sectionStatus();
-      Serial.println(91133);
+    led3GREENflashing = 1;            // SEMAPHOR TRACK 03 GREEN START FLASHING
+    digitalWrite(led3RED, LOW);       // SEMAPHOR TRACK 03 RED OFF
+    Serial.println(91133);
     }
-  }
   
 } // end void sensorLow()
 
@@ -123,84 +94,75 @@ void TxRxData() {               // TxRxData TRANSMISION
 } // end void TxRxData()
 
 void TRACK_01_setUp(){          // TRACK 01 ON-LINE -> 91110
-  if (trackStatus[0] == 1){
-      trackStatus[0] = 0;
-      led1GREENflashing = 0;                                  // SEMAPHOR TRACK 01 GREEN STOP FLASHINK
-      digitalWrite(led1RED,   HIGH);                          // SEMAPHOR TRACK 01 RED   OFF
-      digitalWrite(led2RED,   LOW);                           // SEMAPHOR TRACK 02 RED   ON
-      digitalWrite(led3RED,   LOW);                           // SEMAPHOR TRACK 03 RED   ON
-      digitalWrite(led1GREEN, LOW);                           // SEMAPHOR TRACK 01 GREEN ON
-      digitalWrite(led2GREEN, HIGH);                          // SEMAPHOR TRACK 02 GREEN OFF
-      digitalWrite(led3GREEN, HIGH);                          // SEMAPHOR TRACK 03 GREEN OFF
-      servoL.attach(servoL_Pin);                              // TUTN ON SERVO L
-      servoR.attach(servoR_Pin);                              // TUTN ON SERVO R
-      for (posServoL = 100; posServoL > 58; posServoL -= 1){  // SERVO L ON THE POSITION
-        servoL.write(posServoL);                              // SERVO L STRAIGHT
-      }
-      for (posServoR = 100; posServoR > 58; posServoR -= 1){  // SERVO R ON THE POSITION
-        servoR.write(posServoR);                              // SERVO R ASIDE
-      }
-      delay(100);
-      servoL.detach();                                        // TUTN OFF SERVO L
-      servoR.detach();                                        // TUTN OFF SERVO R
-      Serial.print("TRACK 01 SWITCHED ON-LINE | ");
-      sectionStatus();
-      Serial.println(91110);
-  }
+
+  led1GREENflashing = 0;                                  // SEMAPHOR TRACK 01 GREEN STOP FLASHINK
+  digitalWrite(led1RED,   HIGH);                          // SEMAPHOR TRACK 01 RED   OFF
+  digitalWrite(led2RED,   LOW);                           // SEMAPHOR TRACK 02 RED   ON
+  digitalWrite(led3RED,   LOW);                           // SEMAPHOR TRACK 03 RED   ON
+  digitalWrite(led1GREEN, LOW);                           // SEMAPHOR TRACK 01 GREEN ON
+  digitalWrite(led2GREEN, HIGH);                          // SEMAPHOR TRACK 02 GREEN OFF
+  digitalWrite(led3GREEN, HIGH);                          // SEMAPHOR TRACK 03 GREEN OFF
+  servoL.attach(servoL_Pin);                              // TUTN ON SERVO L
+  servoR.attach(servoR_Pin);                              // TUTN ON SERVO R
+  for (posServoL = 100; posServoL > 58; posServoL -= 1){  // SERVO L ON THE POSITION
+    servoL.write(posServoL);                              // SERVO L STRAIGHT
+    }
+  for (posServoR = 100; posServoR > 58; posServoR -= 1){  // SERVO R ON THE POSITION
+    servoR.write(posServoR);                              // SERVO R ASIDE
+    }
+  delay(100);
+  servoL.detach();                                        // TUTN OFF SERVO L
+  servoR.detach();                                        // TUTN OFF SERVO R
+  Serial.println(91110);
+
 } // end void TRACK_01_setUp()
 
 void TRACK_02_setUp(){          // TRACK 02 ON-LINE -> 91120
-  if (trackStatus[1] == 1){
-      trackStatus[1] = 0;
-      led2GREENflashing = 0;                                  // SEMAPHOR TRACK 02 GREEN STOP FLASHINK
-      digitalWrite(led1RED,   LOW);                           // SEMAPHOR TRACK 01 RED   ON
-      digitalWrite(led2RED,   HIGH);                          // SEMAPHOR TRACK 02 RED   OFF
-      digitalWrite(led3RED,   LOW);                           // SEMAPHOR TRACK 03 RED   ON
-      digitalWrite(led1GREEN, HIGH);                          // SEMAPHOR TRACK 01 GREEN OFF
-      digitalWrite(led2GREEN, LOW);                           // SEMAPHOR TRACK 02 GREEN ON
-      digitalWrite(led3GREEN, HIGH);                          // SEMAPHOR TRACK 03 GREEN OFF
-      servoL.attach(servoL_Pin);                              // TUTN ON SERVO L
-      servoR.attach(servoR_Pin);                              // TUTN ON SERVO R
-      for (posServoL = 100; posServoL > 58; posServoL -= 1) { // SERVO L ON THE POSITION
-      servoL.write(posServoL);                                // SERVO L STRAIGHT
-      }
-      for (posServoR = 58; posServoR < 100; posServoR += 1) { // SERVO R ON THE POSITION
-        servoR.write(posServoR);                              // SERVO R STRAIGHT
-      }
-      delay(100);
-      servoL.detach();                                        // TUTN OFF SERVO L
-      servoR.detach();                                        // TUTN OFF SERVO R
-      Serial.print("TRACK 02 SWITCHED ON-LINE | ");
-      sectionStatus();
-      Serial.println(91120);
-  }
+
+  led2GREENflashing = 0;                                  // SEMAPHOR TRACK 02 GREEN STOP FLASHINK
+  digitalWrite(led1RED,   LOW);                           // SEMAPHOR TRACK 01 RED   ON
+  digitalWrite(led2RED,   HIGH);                          // SEMAPHOR TRACK 02 RED   OFF
+  digitalWrite(led3RED,   LOW);                           // SEMAPHOR TRACK 03 RED   ON
+  digitalWrite(led1GREEN, HIGH);                          // SEMAPHOR TRACK 01 GREEN OFF
+  digitalWrite(led2GREEN, LOW);                           // SEMAPHOR TRACK 02 GREEN ON
+  digitalWrite(led3GREEN, HIGH);                          // SEMAPHOR TRACK 03 GREEN OFF
+  servoL.attach(servoL_Pin);                              // TUTN ON SERVO L
+  servoR.attach(servoR_Pin);                              // TUTN ON SERVO R
+  for (posServoL = 100; posServoL > 58; posServoL -= 1) { // SERVO L ON THE POSITION
+    servoL.write(posServoL);                                // SERVO L STRAIGHT
+    }
+  for (posServoR = 58; posServoR < 100; posServoR += 1) { // SERVO R ON THE POSITION
+    servoR.write(posServoR);                              // SERVO R STRAIGHT
+    }
+  delay(100);
+  servoL.detach();                                        // TUTN OFF SERVO L
+  servoR.detach();                                        // TUTN OFF SERVO R
+  Serial.println(91120);
+
 } // end void TRACK_02_setUp()
 
 void TRACK_03_setUp(){          // TRACK 03 ON-LINE -> 91130
-  if (trackStatus[2] == 1){
-      trackStatus[2] = 0;
-      led3GREENflashing = 0;                                  // SEMAPHOR TRACK 03 GREEN STOP FLASHINK
-      digitalWrite(led1RED,   LOW);                           // SEMAPHOR TRACK 01 RED   ON
-      digitalWrite(led2RED,   LOW);                           // SEMAPHOR TRACK 02 RED   ON
-      digitalWrite(led3RED,   HIGH);                          // SEMAPHOR TRACK 03 RED   OFF
-      digitalWrite(led1GREEN, HIGH);                          // SEMAPHOR TRACK 01 GREEN OFF
-      digitalWrite(led2GREEN, HIGH);                          // SEMAPHOR TRACK 02 GREEN OFF
-      digitalWrite(led3GREEN, LOW);                           // SEMAPHOR TRACK 03 GREEN ON
-      servoL.attach(servoL_Pin);                              // TUTN ON SERVO L
-      servoR.attach(servoR_Pin);                              // TUTN ON SERVO R
-      for (posServoL = 58; posServoL < 100; posServoL += 1) { // SERVO L ON THE POSITION
-        servoL.write(posServoL);                              // SERVO L ASIDE
-      }
-      for (posServoR = 58; posServoR < 100; posServoR += 1) { // SERVO R ON THE POSITION
-        servoR.write(posServoR);                              // SERVO R STRAIGHT
-      }
-      delay(100);
-      servoL.detach();                                        // TUTN OFF SERVO L
-      servoR.detach();                                        // TUTN OFF SERVO R
-      Serial.print("TRACK 03 SWITCHED ON-LINE | ");
-      sectionStatus();
-      Serial.println(91130);
-  }
+
+  led3GREENflashing = 0;                                  // SEMAPHOR TRACK 03 GREEN STOP FLASHINK
+  digitalWrite(led1RED,   LOW);                           // SEMAPHOR TRACK 01 RED   ON
+  digitalWrite(led2RED,   LOW);                           // SEMAPHOR TRACK 02 RED   ON
+  digitalWrite(led3RED,   HIGH);                          // SEMAPHOR TRACK 03 RED   OFF
+  digitalWrite(led1GREEN, HIGH);                          // SEMAPHOR TRACK 01 GREEN OFF
+  digitalWrite(led2GREEN, HIGH);                          // SEMAPHOR TRACK 02 GREEN OFF
+  digitalWrite(led3GREEN, LOW);                           // SEMAPHOR TRACK 03 GREEN ON
+  servoL.attach(servoL_Pin);                              // TUTN ON SERVO L
+  servoR.attach(servoR_Pin);                              // TUTN ON SERVO R
+  for (posServoL = 58; posServoL < 100; posServoL += 1) { // SERVO L ON THE POSITION
+    servoL.write(posServoL);                              // SERVO L ASIDE
+    }
+  for (posServoR = 58; posServoR < 100; posServoR += 1) { // SERVO R ON THE POSITION
+    servoR.write(posServoR);                              // SERVO R STRAIGHT
+    }
+  delay(100);
+  servoL.detach();                                        // TUTN OFF SERVO L
+  servoR.detach();                                        // TUTN OFF SERVO R
+  Serial.println(91130);
+
 } // end void TRACK_03_setUp()
 
 void loop() {                   // WORK IN LOOP
